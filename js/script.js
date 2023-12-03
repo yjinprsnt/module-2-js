02064ec35767f2da04e083c0d331872ff138a0b8
Evgeniy Yurchenko <125579639+yjinprsnt@users.noreply.github.com>
2023-12-03 21:42:17 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,370 @@
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// function sayHi() {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi("Function Declaration");
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+//   return a + b + c;
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// function makeMessage(name, price) {
+//   // Change code below this line
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// }
+
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+//   // Change code above this line
+//   return totalPrice;
+// }
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+
+//   const message = `You ordered droids worth ${
+//     orderedQuantity * pricePerDroid + deliveryFee
+//   } credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   // Change code above this line
+//   return message;
+// }
+
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
+
+//   // Change code above this line
+//   return passed;
+// }
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//   const isMatch = password === "jqueryismyjam";
+
+//   // Change code above this line
+//   return isMatch;
+// }
+
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     // Change this line
+//     message = "You are an adult";
+//   } else {
+//     message = "You are a minor";
+//   }
+
+//   return message;
+// }
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (available >= ordered) {
+//     message = "Order is processed, our manager will contact you.";
+//   } else {
+//     message = "Not enough goods in stock!";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+
+//   if (totalPrice <= customerCredits) {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   } else {
+//     message = "Insufficient funds!";
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === null) {
+//     // Change this line
+//     message = 'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) {
+//     // Change this line
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+
+//   return message;
+// }
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+
+//   if (ordered === 0) {
+//     message = 'There are no products in the order!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end; // Change this line
+
+//   return isInRange;
+// }
+
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === 'pro' || subType === 'vip'; // Change this line
+
+//   return canAccessContent;
+// }
+
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange; // Change this line
+
+//   return isNotInRange;
+// }
+
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else {
+//     discount = BASE_DISCOUNT;
+//   }
+
+//   // Change code above this line
+//   return discount;
+// }
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   let message;
+//   message =
+//     ordered > available
+//       ? 'Not enough goods in stock!'
+//       : 'The order is accepted, our manager will contact you';
+
+//   return message;
+// }
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   message =
+//     password === ADMIN_PASSWORD
+//       ? 'Access is allowed'
+//       : 'Access denied, wrong password!';
+//   // Change code below this line
+
+//   // Change code above this line
+//   return message;
+// }
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//   switch (
+//     type // Change this line
+//   ) {
+//     case 'starter':
+//       price = 0; // Change this line
+//       break;
+
+//     case 'professional': // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case 'organization': // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   // Change code below this line
+//   switch (password) {
+//     case null:
+//       message = 'Canceled by user!';
+//       break;
+
+//     case ADMIN_PASSWORD:
+//       message = 'Welcome!';
+//       break;
+
+//     default:
+//       message = 'Access denied, wrong password!';
+//   }
+
+//   // if (password === null) {
+//   //   message = 'Canceled by user!';
+//   // } else if (password === ADMIN_PASSWORD) {
+//   //   message = 'Welcome!';
+//   // } else {
+//   //   message = 'Access denied, wrong password!';
+//   // }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+//   switch (country) {
+//     case 'China':
+//       message = `Shipping to China will cost 100 credits`;
+//       break;
+
+//     case 'Chile':
+//       message = `Shipping to Chile will cost 250 credits`;
+//       break;
+
+//     case 'Australia':
+//       message = `Shipping to Australia will cost 170 credits`;
+//       break;
+
+//     case 'Jamaica':
+//       message = `Shipping to Jamaica will cost 120 credits`;
+//       break;
+
+//     default:
+//       message = 'Sorry, there is no delivery to your country';
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+//   return message;
+// }
+
+// const courseTopic = 'JavaScript essentials';
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length - 1];
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(string, length);
+
+//   return substring;
+// }
+
+// function formatMessage(message, maxLength) {
+//   let result;
+
+//   result =
+//     message.length > maxLength ? message.slice(0, maxLength) + '...' : message;
+//   /// Change code above this line
+//   return result;
+// }
+
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase(); // Change this line
+
+//   return normalizedInput;
+// }
+
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Change this line
+//   return result;
+// }
+
+// function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+//   const normalizeMessage = message.toLowerCase();
+//   result =
+//     normalizeMessage.includes('spam') || normalizeMessage.includes('sale');
+
+//   // Change code above this line
+//   return result;
+// }
