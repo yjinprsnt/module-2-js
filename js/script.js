78d8a4dfc0bf98892b8248b4472525b363073f07
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2023-12-30 23:21:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -423,3 +423,9 @@
 //   // Change code above this line
 // }
 
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
