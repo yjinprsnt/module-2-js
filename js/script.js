8284628eb2e711bdb54348c5247ced4d8a82258a
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2023-12-30 23:20:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -410,3 +410,16 @@
 //   // Change code above this line
 // }
 
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+//   // Change code above this line
+// }
+
