a6721058cdba64428c6a3b97a924629f5493301e
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-01 15:49:04 +0200

    add 14 auto check js

--- js/script.js
+++ js/script.js
@@ -472,3 +472,17 @@
 //   return string;
 // }
 
+// function slugify(title) {
+//   return title.toLowerCase().split(' ').join('-');
+// }
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, -1);
+// const lastThreeEls = fruits.slice(-3);
+
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
