43cd096aa0922bee4e7b04cc54cc040815306be7
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-02 17:06:01 +0200

    add 18 auto check js

--- js/script.js
+++ js/script.js
@@ -516,3 +516,12 @@
 //   console.log(i);
 // }
 
+// function calculateTotal(number) {
+//   let total = 0;
+//   for (let i = 0; i <= number; i += 1) {
+//     total += i;
+//   }
+//   return total;
+// }
+// calculateTotal(1);
+
