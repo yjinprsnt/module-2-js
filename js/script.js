e90f46b0578cf3ce4554a1cdb439142a6872cef6
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-02 20:16:58 +0200

    add 31 auto check js

--- js/script.js
+++ js/script.js
@@ -677,3 +677,17 @@
 //   }
 // }
 
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       return i;
+//     }
+//   }
+
+//   return number;
+//   // Change code above this line
+// }
+
