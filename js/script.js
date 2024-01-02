af40642dfff96abe14136544b552607ad535735c
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-02 17:05:43 +0200

    add 17 auto check js

--- js/script.js
+++ js/script.js
@@ -508,3 +508,11 @@
 //   return allArray.slice(0, maxLength);
 // }
 
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   // Change this line
+//   console.log(i);
+// }
+
