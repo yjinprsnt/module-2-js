437cc26c88c9b2b0095e9b9ea101fe83098ab62e
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-02 17:06:15 +0200

    add 19 auto check js

--- js/script.js
+++ js/script.js
@@ -525,3 +525,11 @@
 // }
 // calculateTotal(1);
 
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
