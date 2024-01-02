c818f1d565f08f84b2ed7f25aeef30011a214faa
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-02 20:14:54 +0200

    add 27 auto check js

--- js/script.js
+++ js/script.js
@@ -621,3 +621,26 @@
 //   return total;
 // }
 
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   const number = numbers[i];
+
+//   //   if (number > value) {
+//   //     filteredNumbers.push(number);
+//   //   }
+//   // }
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+
+// // Change code below this line
